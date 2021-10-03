e656c0d959cac0b615a974def87f4dcc11393577
esmattoha <mondaldipu904@gmail.com>
2021-10-03 10:09:52 +0530

    change : logic add one more poperties

--- order/orderController.js
+++ order/orderController.js
@@ -9,7 +9,7 @@ const totalAmount = async (products) => {
   const reducer = (accumulator, current) => accumulator + current;
 
   const itemprices = products.map(async (item) => {
-    return item.price;
+    return item.price * item.quantity;
   });
 
   const productItemsPrices = await Promise.all(itemprices);
@@ -17,13 +17,23 @@ const totalAmount = async (products) => {
 };
 
 /**
- * 
- * @param {*} products 
+ *
+ * @param {*} products
  * @returns array of products
  */
 const productResources = async (products) => {
   const productItems = await products.map(async (item) => {
-    return await Product.findById(item).select("_id title price");
+    const productResource = await Product.findById(item._id).select(
+      "_id title price"
+    );
+    const productQuantity = item.quantity;
+
+    return {
+      _id: productResource._id,
+      title: productResource.title,
+      price: productResource.price,
+      quantity: productQuantity,
+    };
   });
 
   const productItemsData = await Promise.all(productItems);
