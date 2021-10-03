77f27f29566bad73747e5cc3b981e2313785a940
esmattoha <mondaldipu904@gmail.com>
2021-10-03 10:08:22 +0530

    Recode : Order Model

--- order/orderModel.js
+++ order/orderModel.js
@@ -7,47 +7,37 @@ const { Schema } = mongoose;
 const orderSchema = new Schema(
   {
     user: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Types.ObjectId,
       ref: "User",
-      required: [true, "User is required."],
+      required: true,
     },
     products: [
       {
-        _id : {
-          type: mongoose.Types.ObjectId
+        _id: {
+          type: mongoose.Types.ObjectId,
+          required: true,
         },
-        title:{
-          type: String
+        title: {
+          type: String,
+          required: true,
         },
-        quantity : {
-          type : Number
+        price: {
+          type: Number,
+          required: true,
         },
-        price :{
-          type : Number
-        }
+        quantity: Number,
       },
     ],
-    status : {
-        type : String,
-        default : "hold",
-        enum: ["hold", "comfirmed", "canceled", "complete"]
-    },
-    total_price : Number,
     user_note: String,
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
   },
   {
-    toJSON : true,
-    toObject: true,
     timestamps: true,
   }
 );
-
-// Virtual populate
-orderSchema.virtual("uder",{
-    ref: "User",
-    localField :  "user",
-    foreignField : "_id"
-});
 
 // export
 const Order = mongoose.model("Order", orderSchema);
